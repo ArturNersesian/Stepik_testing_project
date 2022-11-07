99ab67e49329e65ae6bc61df457aa052c9ab873c
Artur <109510338+ArturNersesian@users.noreply.github.com>
2022-11-07 13:53:51 +0300

    should_be_login_link() method added

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,20 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url,timeout=10):
         self.browser = browser
         self.url = url
-
+        self.browser.implicitly_wait(timeout)
+    # открывает страницу переданную в url
     def open(self):
         self.browser.get(self.url)
+    # Проверяет присутствует ли элемент на странице (возвращает True или False)
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+
